c04fa99fa77b7a53be2a75e72f6f7f7310ca1d34
Mike Miles <MMiles@genuineinteractive.com>
2016-07-12 17:17:17 -0400

    Remvoed Guzzle, don't need it. Added beginging of metadata parsing

--- src/AppBundle/Utils/ULParser.php
+++ src/AppBundle/Utils/ULParser.php
@@ -2,21 +2,15 @@
 
 namespace AppBundle\Utils
 
-use EightPoints\Bundle\GuzzleBundle\GuzzleBundle;
 use Symfony\Component\DomCrawler\Crawler;
-use AppBundle\Utils\ULDatabaseInterface;
 
-class ULParser implements ULParserInterface {
+class ULParser {
 
-  private $crawler;
   private $database;
-  private $guzzle;
   private $max_age = 86400;
 
-  public function __construct(Crawler $crawler, GuzzleBundle $guzzle, ULDatabaseInterface $database) {
+  public function __construct(ULDatabaseInterface $database) {
 
-    $this->crawler = $crawler;
-    $this->guzzle = $guzzle
     $this->database = $database;
     return $this;
   }
@@ -56,9 +50,7 @@ class ULParser implements ULParserInterface {
             // Update the content document.
             $content_document->update();
           }
-
         }
-
       }
     }
   }
@@ -77,13 +69,38 @@ class ULParser implements ULParserInterface {
   private function fetchContent($url) {
     $html = null;
 
+    $ch = curl_init();
+    $timeout = 5;
+    curl_setopt($ch, CURLOPT_URL, $url);
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+    curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, $timeout);
+    $html = curl_exec($ch);
+    curl_close($ch);
+
     return $html;
   }
 
   private function parseContentData($html, $document_type) {
-    $parsed_data = null;
+    $parsed_data = array();
 
-    return $parsed_data;
+    $crawler = new Crawler($html);
+
+    foreach ($document_type->field_mappings as $field) {
+      $field_content = $crawler->filter($field->selector)->each(function (Crawler $node, $i) {
+        return $node->text();
+      });
+
+      if (!empty($field_content)) {
+        $parsed_data[] = [
+          'field' => $field->machine_name,
+          'selector' => $field->selector,
+          'data' => $this->sanitizeContent($field_content),
+        ];
+      }
+    }
+
+
+    return empty($parsed_data) ? null:$parsed_data;
   }
 
   private function contentChanged($content_a, $content_b) {
@@ -91,7 +108,21 @@ class ULParser implements ULParserInterface {
   }
 
   private function updateMetaData(&$content_document) {
+    // Create a 'metadata' document type.
+    $metadata_type = (object)[
+      ['machine_name' => 'keywords', 'selector' => 'meta [name|=Keywords]'],
+      ['machine_name' => 'description', 'selector' => 'meta [name|=Description'],
+    ];
 
+
+    if ($metadata = $this->parseContentData($content_document->raw_content, $metadata_type)) {
+      // Append to existing meta data.
+    }
+
+  }
+
+  private function sanitizeContent($content) {
+    return strip_tags($content);
   }
 
 }
