8c659fc90cf0e6432d4d5e0c90aab4b2604e70eb
charginghawk <tcharginghawk@wearegenuine.com>
2016-07-13 17:11:09 -0400

    Updating ULSiteConfig, adding ULContentDocumentType.

---
+++ src/AppBundle/Document/ULContentDocumentType.php
@@ -0,0 +1,134 @@
+<?php
+
+namespace AppBundle\Document;
+
+use Doctrine\ODM\MongoDB\Mapping\Annotations as MongoDB;
+
+/**
+ * @MongoDB\Document
+ */
+class ULContentDocumentType {
+
+    /**
+     * @MongoDB\Id
+     */
+    private $content_document_type_id;
+
+    /**
+     * @MongoDB\Field(type="string")
+     */
+    private $label;
+
+    /**
+     * @MongoDB\Field(type="string")
+     */
+    private $description;
+
+    /**
+     * @MongoDB\Field(type="collection")
+     */
+    private $fields;
+
+    /**
+     * @MongoDB\Field(type="collection")
+     */
+    private $markup_types;
+
+    /**
+     * Get contentDocumentTypeId
+     *
+     * @return id $contentDocumentTypeId
+     */
+    public function getContentDocumentTypeId()
+    {
+        return $this->content_document_type_id;
+    }
+
+    /**
+     * Set label
+     *
+     * @param string $label
+     * @return $this
+     */
+    public function setLabel($label)
+    {
+        $this->label = $label;
+        return $this;
+    }
+
+    /**
+     * Get label
+     *
+     * @return string $label
+     */
+    public function getLabel()
+    {
+        return $this->label;
+    }
+
+    /**
+     * Set description
+     *
+     * @param string $description
+     * @return $this
+     */
+    public function setDescription($description)
+    {
+        $this->description = $description;
+        return $this;
+    }
+
+    /**
+     * Get description
+     *
+     * @return string $description
+     */
+    public function getDescription()
+    {
+        return $this->description;
+    }
+
+    /**
+     * Set fields
+     *
+     * @param collection $fields
+     * @return $this
+     */
+    public function setFields($fields)
+    {
+        $this->fields = $fields;
+        return $this;
+    }
+
+    /**
+     * Get fields
+     *
+     * @return collection $fields
+     */
+    public function getFields()
+    {
+        return $this->fields;
+    }
+
+    /**
+     * Set markupTypes
+     *
+     * @param collection $markupTypes
+     * @return $this
+     */
+    public function setMarkupTypes($markupTypes)
+    {
+        $this->markup_types = $markupTypes;
+        return $this;
+    }
+
+    /**
+     * Get markupTypes
+     *
+     * @return collection $markupTypes
+     */
+    public function getMarkupTypes()
+    {
+        return $this->markup_types;
+    }
+}
--- src/AppBundle/Document/ULSiteConfig.php
+++ src/AppBundle/Document/ULSiteConfig.php
@@ -9,15 +9,45 @@ use Doctrine\ODM\MongoDB\Mapping\Annotations as MongoDB;
  */
 class ULSiteConfig implements ULSiteConfigInterface {
 
-  /**
-   * @MongoDB\Id
-   */
-  private $site_config_id;
+    /**
+     * @MongoDB\Id
+     */
+    private $site_config_id;
 
-  public function getDocumentTypeInstances(){
+    /**
+     * @MongoDB\Field(type="string")
+     */
+    private $label;
 
-  }
+    /**
+     * @MongoDB\Field(type="string")
+     */
+    private $site_domain;
 
+    /**
+     * @MongoDB\Field(type="timestamp")
+     */
+    private $last_update_date;
+
+    /**
+     * @MongoDB\Field(type="string")
+     */
+    private $discovery_type;
+
+    /**
+     * @MongoDB\Field(type="boolean")
+     */
+    private $lock_status;
+
+    /**
+     * @MongoDB\Field(type="collection")
+     */
+    private $metadata;
+
+    /**
+     * @MongoDB\Field(type="collection")
+     */
+    private $document_type_instances;
 
     /**
      * Get siteConfigId
@@ -27,5 +57,158 @@ class ULSiteConfig implements ULSiteConfigInterface {
     public function getSiteConfigId()
     {
         return $this->site_config_id;
+    }
+
+    /**
+     * Set label
+     *
+     * @param string $label
+     * @return $this
+     */
+    public function setLabel($label)
+    {
+        $this->label = $label;
+        return $this;
+    }
+
+    /**
+     * Get label
+     *
+     * @return string $label
+     */
+    public function getLabel()
+    {
+        return $this->label;
+    }
+
+    /**
+     * Set siteDomain
+     *
+     * @param string $siteDomain
+     * @return $this
+     */
+    public function setSiteDomain($siteDomain)
+    {
+        $this->site_domain = $siteDomain;
+        return $this;
+    }
+
+    /**
+     * Get siteDomain
+     *
+     * @return string $siteDomain
+     */
+    public function getSiteDomain()
+    {
+        return $this->site_domain;
+    }
+
+    /**
+     * Set lastUpdateDate
+     *
+     * @param timestamp $lastUpdateDate
+     * @return $this
+     */
+    public function setLastUpdateDate($lastUpdateDate)
+    {
+        $this->last_update_date = $lastUpdateDate;
+        return $this;
+    }
+
+    /**
+     * Get lastUpdateDate
+     *
+     * @return timestamp $lastUpdateDate
+     */
+    public function getLastUpdateDate()
+    {
+        return $this->last_update_date;
+    }
+
+    /**
+     * Set discoveryType
+     *
+     * @param string $discoveryType
+     * @return $this
+     */
+    public function setDiscoveryType($discoveryType)
+    {
+        $this->discovery_type = $discoveryType;
+        return $this;
+    }
+
+    /**
+     * Get discoveryType
+     *
+     * @return string $discoveryType
+     */
+    public function getDiscoveryType()
+    {
+        return $this->discovery_type;
+    }
+
+    /**
+     * Set lockStatus
+     *
+     * @param boolean $lockStatus
+     * @return $this
+     */
+    public function setLockStatus($lockStatus)
+    {
+        $this->lock_status = $lockStatus;
+        return $this;
+    }
+
+    /**
+     * Get lockStatus
+     *
+     * @return boolean $lockStatus
+     */
+    public function getLockStatus()
+    {
+        return $this->lock_status;
+    }
+
+    /**
+     * Set metadata
+     *
+     * @param collection $metadata
+     * @return $this
+     */
+    public function setMetadata($metadata)
+    {
+        $this->metadata = $metadata;
+        return $this;
+    }
+
+    /**
+     * Get metadata
+     *
+     * @return collection $metadata
+     */
+    public function getMetadata()
+    {
+        return $this->metadata;
+    }
+
+    /**
+     * Set documentTypeInstances
+     *
+     * @param collection $documentTypeInstances
+     * @return $this
+     */
+    public function setDocumentTypeInstances($documentTypeInstances)
+    {
+        $this->document_type_instances = $documentTypeInstances;
+        return $this;
+    }
+
+    /**
+     * Set documentTypeInstances
+     *
+     * @return collection $documentTypeInstances
+     */
+    public function getDocumentTypeInstances(){
+      return $this->document_type_instances;
     }
 }
